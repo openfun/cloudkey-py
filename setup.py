4f5e6338fed93d8163d3f0aef42c7ba68d7cf7ae
Sebastien Estienne <sebastien.estienne@gmail.com>
2011-07-16 01:46:58 +0200

    Many improvements from Kurt Bendl

--- setup.py
+++ setup.py
@@ -1,15 +1,31 @@
-#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os, sys
 
-from cloudkey import __version__ as version
-from setuptools import setup
-
+version = open("cloudkey/version.txt").read().strip()
 setup(name='cloudkey',
-      description='Dailymotion Cloud API client library',
-      author='Dailymotion',
-      url='http://github.com/dailymotion/cloudkey-py',
       version=version,
+      description='Dailymotion Cloud API client library',
+      long_description=open("README.txt").read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        ],
+      keywords=['dailymotion', 'dmcloud', 'cloud', 'cloudkey', 'api', 'sdk'],
+      author='Sebastien Estienne',
+      author_email='sebastien.estienne@gmail.com',
+      url='http://github.com/dailymotion/cloudkey-py',
+      license='Apache License, Version 2.0',
+      include_package_data=True,
+      zip_safe=False,
       py_modules = ['cloudkey',],
-      install_requires=['simplejson>=2.0.9', 'pycurl>=7.19.0'],
-      zip_safe=True,
-      test_suite='tests')
-
+      install_requires=[
+          'setuptools',
+          'simplejson>=2.0.9',
+          'pycurl>=7.19.0',
+      ],
+)
