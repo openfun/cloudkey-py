57a8d1db3175432adb0546ef4187c7832061b965
Jerome Clerc <jererc@gmail.com>
2011-11-23 17:13:37 +0100

    fixed version parameter for static also

--- cloudkey.py
+++ cloudkey.py
@@ -391,14 +391,14 @@ class MediaObject(ClientObject):
     def get_stream_url(self, id, asset_name='mp4_h264_aac', seclevel=None, asnum=None, ip=None, useragent=None, countries=None, referers=None, expires=None, download=False, version=None, cdn_url='http://cdn.dmcloud.net'):
         if type(id) not in (str, unicode):
             raise InvalidParameter('id is not valid')
+        version = '-%d' % version if version else ''
         if asset_name.startswith('jpeg_thumbnail_'):
             base_url = cdn_url.replace('cdn.', 'static.')
             ts = '-%d' % int(expires) if expires else ''
-            return '%s/%s/%s/%s%s.jpeg' % (base_url, self._client._user_id, id, asset_name, ts)
+            return '%s/%s/%s/%s%s%s.jpeg' % (base_url, self._client._user_id, id, asset_name, ts, version)
         extension = asset_name.split('_')[0]
         if extension == 'f4f':
             extension = 'f4m'
-        version = '-%d' % version if version else ''
         url = '%s/route/%s/%s/%s%s.%s' % (cdn_url, self._client._user_id, id, asset_name, version, extension)
         return sign_url(url, self._client._api_key, seclevel=seclevel, asnum=asnum, ip=ip, useragent=useragent, countries=countries, referers=referers, expires=expires) \
             + ('&throttle=0&helper=0&cache=0' if download else '')
